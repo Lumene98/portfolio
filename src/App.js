65f496de6213dc3a6db077dcca7269803dcd5c76
Luca Meneghini <lucamene98@outlook.it>
2019-03-25 13:38:31 +0100

    💄 Add Icon

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ class App extends Component {
         <ClickBurst>
           <a href="#">Link</a>
         </ClickBurst>
+        <div className='footer'>Icons made by <a href="https://www.freepik.com/" title="Freepik" target="_blank" rel='noopener noreferrer' >Freepik</a> from <a
+          href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel='noopener noreferrer' >www.flaticon.com</a> is licensed by <a
+            href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel='noopener noreferrer'>CC 3.0 BY</a>
+        </div>
       </div>
     );
   }
