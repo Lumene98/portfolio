f0922def75ef8cedd025e83df782835afa2178b3
Luca Meneghini <lucamene98@outlook.it>
2019-03-26 10:33:34 +0100

    💄 Fix styling and Rocket Icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export class Footer extends Component {
     render() {
         return (
-            <div>
-                <div className='footer'>Icons made by <a href="https://www.freepik.com/" title="Freepik" target="_blank" rel='noopener noreferrer' >Freepik</a> from <a
+            <div style={{ display: 'flex' }}>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'center', fontSize: '0.8em' }} className='footer'>Icons made by <a href="https://www.freepik.com/" title="Freepik" target="_blank" rel='noopener noreferrer' >Freepik</a> from <a
                     href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel='noopener noreferrer' >www.flaticon.com</a> is licensed by <a
                         href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank" rel='noopener noreferrer'>CC 3.0 BY</a>
                 </div>
