181b6357693b71ece4091ca1ef8bff68ce90e8ee
Luca Meneghini <lucamene98@outlook.it>
2020-11-19 18:12:35 +0100

    Updates

---
+++ precache-manifest.1fa22f4283694c3b2a6b2cc6a65a683e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1ebc0d4b11b4eb23f83",
+    "url": "/portfolio/static/js/main.c6ca9d5d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "a5e644c9d7b0ef58bc70",
+    "url": "/portfolio/static/js/2.23e2f010.chunk.js"
+  },
+  {
+    "revision": "5690a542bedbdbec5178c1304e6bd6c6",
+    "url": "/portfolio/index.html"
+  }
+];
