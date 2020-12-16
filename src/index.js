6bbd980c99ebdb20c8597f0ee679184675e8d422
Luca Meneghini <lucameneghini98@outlook.it>
2020-12-16 15:15:01 +0100

    :fire: Remove old GlobalStyle

--- src/index.js
+++ src/index.js
@@ -2,36 +2,13 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createGlobalStyle } from "styled-components/macro";
 import * as serviceWorker from "./serviceWorker";
 
 const mountPoint = document.getElementById("root");
 
-const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: Fredoka One, regular;
-    font-style: normal;
-    font-weight: 400;
-    
-  }
-  
-  body {
-    font-family: Fredoka One, sans-serif;
-    background: dodgerblue;
-    color: white;
-    display: flex;
-    height: 100vh;
-    width: 100vw;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-  }
-`;
-
 export const applyGlobalStyle = (Element: React$ComponentType<any>) => (
   <Fragment>
     <Element></Element>
-    <GlobalStyles />
   </Fragment>
 );
 
