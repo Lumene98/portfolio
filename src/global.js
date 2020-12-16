7ddcf2a1843b1722ca808ec48c4a2cc76c0e07f4
Luca Meneghini <lucameneghini98@outlook.it>
2020-12-16 15:14:23 +0100

    :lipstick: Adds GlobalStyle

---
+++ src/global.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components/macro";
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  @font-face {
+    font-family: Fredoka One, regular;
+    font-style: normal;
+    font-weight: 400;
+  }
+
+  body {
+    align-items: center;
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+    font-family: Fredoka One,sans-serif;
+    transition: all 0.25s linear;
+    }
+`;
